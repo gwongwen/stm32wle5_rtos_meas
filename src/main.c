c1b20aee9ab9ee2c148e2b0ffc94bbe2edf54266
gwongwen <gwongwen.trash@gmail.com>
2024-06-11 15:53:14 +0200

    change timer for adc and not vbat

--- src/main.c
+++ src/main.c
@@ -3,28 +3,30 @@
 #include "app_adc.h"
 #include "app_vbat.h"
 
+//  ======== globals ============================================
+uint16_t cnt;           // declaration of counter index
+
 //  ======== interrupt sub-routine ===============================
-void vbat_work_handler(struct k_timer *work)
+void adc_work_handler(struct k_timer *work)
 {
-	const struct device *dev;
-	uint16_t vbat;
-
-	vbat = app_stm32_get_vbat(dev);
-	printk("stm32 vbat: %d\n", vbat);
+	uint16_t value;
+	value = app_adc_get_val();
+	cnt++;
 }
-K_WORK_DEFINE(vbat_work, vbat_work_handler);
+K_WORK_DEFINE(adc_work, adc_work_handler);
 
-void vbat_timer_handler(struct k_timer *timer)
+void adc_timer_handler(struct k_timer *timer)
 {
-	k_work_submit(&vbat_work);
+	k_work_submit(&adc_work);
 }
-K_TIMER_DEFINE(vbat_timer, vbat_timer_handler, NULL);
+K_TIMER_DEFINE(adc_timer, adc_timer_handler, NULL);
 
 //  ======== main ===============================================
 int8_t main(void)
 {
 	const struct device *dev;
-	uint16_t value;
+	uint16_t vbat;
+	cnt = 0;
 
 	// initialization of all devices
 	app_stm32_vbat_init(dev);
@@ -32,11 +34,16 @@ int8_t main(void)
 	printk("Battery Level Measurement\nBoard: %s\n", CONFIG_BOARD);
 
 	// beginning of isr timer
-	k_timer_start(&vbat_timer, K_NO_WAIT, K_SECONDS(5));		// for test
+	k_timer_start(&adc_timer, K_NO_WAIT, K_MSEC(5));		// for test
+
 	while (1) {
-		value = app_adc_get_val();
-		k_msleep(1000);
-	}
+		// counter value for waiting 10 min -> 600000ms
+		if (cnt >= 600000) {
+			vbat = app_stm32_get_vbat(dev);
+			printk("stm32 vbat: %"PRIu16"\n", vbat);
+			cnt = 0;
+		}	
+	}	
 	return 0;
 }
 
